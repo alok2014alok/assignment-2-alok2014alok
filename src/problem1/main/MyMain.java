fc26a5c3602bf0ba6779b32def72f4ea7fc3db49
Alok kumar upadhyay <alok.upadhyay_cs18@gla.ac.in>
2020-03-28 17:32:49 +0530

    MAin class,create binary tree

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,34 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+
+
+        int i = 7;
+        System.out.println("Please take only 7 inputs");
+        while (i > 0) {
+            try {
+                tree.add(scanner.nextInt());
+            } catch (Exception e) {
+                System.out.println("Error Occurred is \" " + e + " \", so you are going to be kicked-out \n");
+                break;
+            }
+            i--;
+        }
+
+
+        System.out.println("All Left-children are :");
+        tree.printLeftNode();
+        System.out.println("Number of nodes doesn't have left child :");
+        tree.countNotLeft();
+
+
+    }
 }
